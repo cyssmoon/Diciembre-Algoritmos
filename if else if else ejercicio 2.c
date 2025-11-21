fa63bdea0cb527c8e2d4043511ab2dcbd2178ab7
Luna Estanga <yasestanga@gmail.com>
2025-11-21 12:39:42 -0300

    Update if else if else ejercicio 2.c

--- if else if else ejercicio 2.c
+++ if else if else ejercicio 2.c
@@ -4,10 +4,11 @@ int edad;
 main(){
 	printf("Ingrese su edad:");
 	scanf("%d",&edad);
-	if(edad <= 18){
+	if(edad >= 18){
 		printf("Es mayor de edad");
 	}
 	else{
 		printf("No es mayor de edad");
 	}
 }
+
