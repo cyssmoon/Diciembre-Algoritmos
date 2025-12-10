9d78c823cf2b5a4def185fb1b3fbd654d8e09143
Luna Estanga <yasestanga@gmail.com>
2025-12-10 17:52:53 -0300

    Create matrices.cpp

---
+++ matrices.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+int pasa[3][3], i, j, N=3, acum=0;
+float prom;
+main(){
+	//CARGA
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			printf("Ingrese pasajeros al destino %d en el dia %d ", i,j);
+			scanf("%d", &pasa[i][j]);
+		}
+	}
+	// 1.personas por destino (filas)
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			acum=acum+pasa[i][j];
+		}
+		printf("Total de personas del destino %d es %d \n", i, acum);
+		acum=0;
+		
+	}
+
+    // 2.promedio por dia (columnas)
+	for(j=0; j<N; j++){
+		for(i=0; i<N; i++){
+			acum=acum+pasa[i][j];
+		}
+		prom=acum/N;
+		printf("El promedio de personas del dia %d es %f \n", j, prom);
+		acum=0;
+		
+	}
+	
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			if( (i==0 && j==0)|| pasa[i][j]>max ){
+				max=pasa[i][j];
+				pos_i=i;
+				pos_j=j;
+			}
+			if(i==j){
+				traza=taza+pasa[i][j];
+			}
+		}		
+	}
+	printf("El maximo es y se encuenta en %d i= %d j= %d \n", max, pos_i, pos_j);
+	printf("La traza es %d", traza);
+	
+	for(j=0; j<N; j++){
+		for(i=0; i<N; i++){
+			if((i==0)|| pasa[i][j]<min ){
+				min=pasa[i][j];
+				dia_min=i;
+			}
+		}		
+		printf("El dia %d hubo menos pasajeros\n",dia_min);
+	
+	}
+	
+}
